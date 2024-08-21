fa7a5b4d03ae379fb62e25e76898765377becf2b
Umaidahmad01 <ahmedumaid707@gmail.com>
2024-08-21 15:02:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "20718334")
     API_HASH  = os.environ.get("API_HASH", "4e81464b29d79c58d0ad8a0c55ece4a5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7313790568:AAHFz7x2bX1M4eiE91W-Y_vtatHkSXYp33o") 
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7502313988:AAGjo1bELXhu3xW5nyFHNpzzZL1yNHlYI38") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","Cluster0")     
